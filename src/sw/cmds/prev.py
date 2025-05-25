f23d08d628c493e8023081179ac4baab04c258ea
Anton Van Assche <vanasscheanton@gmail.com>
2025-05-25 20:04:51 +0200

    [prev] Improve error handling

--- src/sw/cmds/prev.py
+++ src/sw/cmds/prev.py
@@ -2,9 +2,9 @@
 
 import click
 
-from sw.core.history import HistoryManager
-from sw.core.wallpaper import WallpaperManager
-from sw.utils.common import log
+from sw.core.history import HistoryManager, HistoryReadError, HistoryWriteError
+from sw.core.wallpaper import InvalidImageError, SubprocessError, WallpaperError, WallpaperManager
+from sw.utils.common import err, log
 
 
 @click.command("prev", short_help="Set the previous wallpaper in the queue")
@@ -22,9 +22,24 @@ def prev_cmd(ctx):
     hm = HistoryManager()
 
     try:
-        path = hm.read()[-2].path
-        wallpaper = wm.set_wallpaper(path)
-        if not ctx.obj.get("silent"):
-            log(f"Wallpaper set: {wallpaper or 'from default dir'}", silent=ctx.obj["silent"])
+        history = hm.read()
+    except HistoryReadError as e:
+        err(ctx, "Failed to read wallpaper history", e)
     except Exception as e:
-        raise Exception(f"Error setting wallpaper: {e}") from e
+        err(ctx, "Unexpected error while reading wallpaper history", e)
+
+    if len(history) < 2:
+        err(ctx, "Not enough history to go back to previous wallpaper", Exception("History too short"))
+
+    try:
+        path = history[-2].path
+    except Exception as e:
+        err(ctx, "Failed to get previous wallpaper path", e)
+
+    try:
+        wallpaper = wm.set_wallpaper(path)
+        log(f"Wallpaper set: {wallpaper or 'from default dir'}", silent=ctx.obj.get("silent", False))
+    except (WallpaperError, InvalidImageError, SubprocessError, HistoryWriteError) as e:
+        err(ctx, "Failed to set previous wallpaper", e)
+    except Exception as e:
+        err(ctx, "Unexpected error while setting previous wallpaper", e)
